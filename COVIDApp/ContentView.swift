374f396ed00de687e4c612faee4f7ad82482e1a9
Benjamin Laskaris <laskaris@bu.edu>
2020-09-11 15:02:59 -0400

    Revert "removed excess folders"
    This reverts commit c19ed0810f3e881d08b4050f71f8f851db1413a0.

---
+++ COVIDApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  COVIDApp
+//
+//  Created by Benjamin Laskaris on 9/8/20.
+//  Copyright © 2020 Benjamin Laskaris. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("COVID 19 Dashboard")
+            .font(.title)
+            .foregroundColor(.red)
+//        Text("this is gonna be lit")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
